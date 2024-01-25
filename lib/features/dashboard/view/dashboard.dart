d2d140cb82da49bb5283e02615dd6e69874d2f79
Pär Dahlberg <per.dahlberg@gmail.com>
2024-01-25 21:34:24 +0100

    wip

--- lib/features/dashboard/view/dashboard.dart
+++ lib/features/dashboard/view/dashboard.dart
@@ -1,5 +1,8 @@
 
 import 'package:flutter/material.dart';
+import 'package:signals/signals_flutter.dart';
+import 'package:solana_rewards/features/tasks/repository/task_repository.dart';
+import 'package:solana_rewards/infra/dependencies.dart';
 
 class Dashboard extends StatelessWidget {
 
@@ -9,9 +12,37 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Dashboard2'),
+    final taskRepository = getIt<TaskRepository>();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text('Balance'),
+            Text('\$AD 0.00'),
+            Text('Tasks'),
+            Expanded(
+              child: Watch.builder(
+                builder: (context) {
+                  var list = taskRepository.list;
+                  return ListView.builder(
+                    itemCount: list.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text(list[index].title),
+                      );
+                    },
+                  );
+                }
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/infra/dependencies.dart
+++ lib/infra/dependencies.dart
@@ -1,6 +1,7 @@
 
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:solana_rewards/features/tasks/repository/task_repository.dart';
 import 'package:solana_rewards/infra/app_router.dart';
 
 final getIt = GetIt.instance;
@@ -8,4 +9,5 @@ final getIt = GetIt.instance;
 void initDependencies() {
   getIt.registerSingleton<AppRouter>(AppRouter());
   getIt.registerSingleton<Dio>(Dio());
+  getIt.registerSingleton<TaskRepository>(TaskRepository());
 }
